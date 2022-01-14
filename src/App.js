6239eb18d0ef3c8dec7be23582b60ef5cfbca025
Satyam1203 <nakulbegana191170@gmail.com>
2022-01-15 00:34:47 +0530

    Bootstrapped project directory

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ProfileDetails from "./components/ProfileDetails";
+
+function App() {
+  return (
+    <div className="App">
+      <ProfileDetails />
+    </div>
+  );
+}
+
+export default App;
